951482990edc1aed5b3d5e099f23528c64b3d688
Meet Patel <meetnpatel368@gmail.com>
2021-12-28 02:08:00 -0500

    Update Search.js
    Update Search.js
    
    Update Search.js
    
    Update Search.js
    
    Update Search.js
    
    Update Search.js
    
    Revert "Merge branch 'main' of https://github.com/Meet-forever/CrimeDataAPI"
    
    This reverts commit 4c8980fc1781d12cd1f8e5b28f9647d9fdf4d17d, reversing
    changes made to 151f0cc9a02a71ba05e0c4cb10bd11edd9a3f97e.
    
    Update index.ejs

--- Routes/Search.js
+++ Routes/Search.js
@@ -7,13 +7,17 @@ const Search = (props) =>{
 
     router.get('/', async(req, res)=>{
         const col = db.collection('rawCrimeData');
-        const {limit, neighborhood, occur_day, npu, type: UC2_Literal, zone, occur_day_number} = req.query
+        const {limit, neighborhood, occur_day, npu, type: UC2_Literal, zone, occur_day_num} = req.query
         let query = {}
             if(!limit || parseInt(limit) <= 0 || isNaN(parseInt(limit))){
                 res.status(400).render("404", {error_no: 400});
                 return;
             }
-            if((parseInt(zone) < 1 && parseInt(zone)>7) ||(parseInt(occur_day_number)<1 || parseInt(occur_day_number)>7) ){
+            if(!isNaN(parseInt(zone)) && (parseInt(zone) < 1 || parseInt(zone) > 7)){
+                res.status(400).render("404", {error_no: 400});
+                return;
+            }
+            if(!isNaN(parseInt(occur_day_num)) && (parseInt(occur_day_num) < 1 || parseInt(occur_day_num) > 7)){
                 res.status(400).render("404", {error_no: 400});
                 return;
             }
@@ -22,8 +26,8 @@ const Search = (props) =>{
             if(occur_day) Object.assign(query, {occur_day: occur_day})
             if(npu) Object.assign(query, {npu: npu})
             if(UC2_Literal) Object.assign(query, {UC2_Literal: UC2_Literal})  
-            if(zone) Object.assign(query, {zone: zone}) 
-            if(occur_day_number) Object.assign(query, {occur_day_number: occur_day_number}) 
+            if(zone) Object.assign(query, {zone: Number(zone)}) 
+            if(occur_day_num) Object.assign(query, {occur_day_num: Number(occur_day_num)}) 
         const result = await col.aggregate([{$match: query},{$project: {_id: 0}}]).limit(limitsize).toArray();
             if(result) res.send(result);
             else res.status(400).render("404", {error_no: 400});
